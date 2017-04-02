6b5a19e08d53325d7a1173ce97422f6162dfb302
Guadalupe Camacho <gcamacho079@gmail.com>
2017-04-02 11:09:53 -0700

    Get JSON call to work, add call for fcc user info

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-var twitchStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
+//var twitchStreamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
+var numberOfStreamers;
+var userName;
 
-var whoIsStreaming = {
-  
+var twitchTV = {
+  init: function() {
+    twitchTV.streamCheck();
+  },
+
+  streamCheck: function() {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/users/freecodecamp?callback=?", function(json) {
+      console.log(json);
+      console.log(json.bio);
+    })
+  }
 }
+
+$(document).ready(twitchTV.init);
