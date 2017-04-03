f8d1bbd24b109f4eb4eadac0c87ec224a7b97a6c
Guadalupe Camacho <gcamacho079@gmail.com>
2017-04-03 12:59:34 -0700

    Remove individual functions to troubleshoot scope problem

--- js/script.js
+++ js/script.js
@@ -7,44 +7,33 @@ var userRow;
 var column1;
 var column2;
 var column3;
-var streamStatus;
+var userStatus;
 
-var twitchTV = {
-  init: function() {
-    twitchTV.getUser();
-  },
+$(document).ready(function() {
+  for (i = 0; i < twitchStreamers.length; i++) {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + twitchStreamers[i] + "?callback=?", function(json) {
+      displayName = json.display_name;
+      description = json.bio;
+      imageSource = json.logo;
+      streamLink = "https://www.twitch.tv/" + json.name;
 
-  getUser: function() {
-    for (i = 0; i < twitchStreamers.length; i++) {
-      $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + twitchStreamers[i] + "?callback=?", function(json) {
-        displayName = json.display_name;
-        description = json.bio;
-        imageSource = json.logo;
-        streamLink = "https://www.twitch.tv/" + json.name;
-        twitchTV.checkStatus();
+      column1 = "<div class='col-sm-3'><img src='" + imageSource + "' class='img-responsive img-circle icon'></img></div>";
+      column2 = "<div class='col-sm-4'><p><a href='" + streamLink + "' target='_blank'>" + displayName + "</a></p></div>";
+      column3 = "<div class='col-sm-5'><p>" + description + "</p></div>";
+
+      /*
+      $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + displayName + "?callback=?", function(data) {
+        if (data.stream == null) {
+          userStatus = "offline";
+        }
+        else {
+          userStatus = "online";
+        }
       })
-    }
-  },
+      */
 
-  makeRow: function(x) {
-    column1 = "<div class='col-sm-3'><img src='" + imageSource + "' class='img-responsive img-circle icon'></img></div>"
-    column2 = "<div class='col-sm-4'><p><a href='" + streamLink + "' target='_blank'>" + displayName + "</a></p></div>"
-    column3 = "<div class='col-sm-5'><p>" + description + "</p></div>";
-    userRow = "<div class='row " + x + "'>" + column1 + column2 + column3 + "</div>";
-    $(userRow).appendTo(".streamFeed");
-  },
-
-  checkStatus: function() {
-    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + displayName + "?callback=?", function(json) {
-      if (json.stream == null) {
-        streamStatus = "offline";
-      }
-      else {
-        streamStatus = "online";
-      }
-      twitchTV.makeRow(streamStatus);
+      userRow = "<div class='row'>" + column1 + column2 + column3 + "</div>";
+      $(userRow).appendTo(".streamFeed");
     })
   }
-}
-
-$(document).ready(twitchTV.init);
+})
