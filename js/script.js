bfd292f5007f440d2ed482f6e9dda36bb0fd761c
Guadalupe Camacho <gcamacho079@gmail.com>
2017-04-03 12:18:48 -0700

    Make background work for online/offline; otherwise broken

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ var userRow;
 var column1;
 var column2;
 var column3;
+var streamStatus;
 
 var twitchTV = {
   init: function() {
@@ -20,21 +21,30 @@ var twitchTV = {
         description = json.bio;
         imageSource = json.logo;
         streamLink = "https://www.twitch.tv/" + json.name;
-        console.log(streamLink);
-        console.log(description);
-        twitchTV.makeRow();
+        twitchTV.checkStatus();
       })
     }
   },
 
-  makeRow: function() {
+  makeRow: function(x) {
     column1 = "<div class='col-sm-3'><img src='" + imageSource + "' class='img-responsive img-circle icon'></img></div>"
     column2 = "<div class='col-sm-4'><p><a href='" + streamLink + "' target='_blank'>" + displayName + "</a></p></div>"
-    column3 = "<div class='col-sm-5'><p>" + description + "</p></div>"
-    userRow = "<div class='row'>" + column1 + column2 + column3 + "</div>";
+    column3 = "<div class='col-sm-5'><p>" + description + "</p></div>";
+    userRow = "<div class='row " + x + "'>" + column1 + column2 + column3 + "</div>";
     $(userRow).appendTo(".streamFeed");
-  }
+  },
 
+  checkStatus: function() {
+    $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + displayName + "?callback=?", function(json) {
+      if (json.stream == null) {
+        streamStatus = "offline";
+      }
+      else {
+        streamStatus = "online";
+      }
+      twitchTV.makeRow(streamStatus);
+    })
+  }
 }
 
 $(document).ready(twitchTV.init);
